7f1ba221d76967b31013a25b1ffd39e75d442dcf
shyeon0602 <july8560@naver.com>
2024-08-08 08:55:33 +0900

    modified for directory -> add array loop code

--- js/goorm-lecture/for/script.js
+++ js/goorm-lecture/for/script.js
@@ -1,3 +1,4 @@
+// for/in
 const user = {
   name: "han",
   province: "경기도",
@@ -7,3 +8,21 @@ const user = {
 for (let x in user) {
   console.log(x);
 }
+
+// 배열 loop
+const locations = ["서울", "부산", "경기도", "대구"];
+
+for (let i = 0; i < locations.length; i++) {
+  console.log(locations[i]);
+}
+
+// forEach
+locations.forEach(function (location, i, arr) {
+  console.log(`${i} : ${location}`);
+  console.log(arr);
+});
+
+// map
+locations.map(function (location) {
+  console.log(location);
+});
