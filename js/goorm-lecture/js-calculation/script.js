e478c4bc4fe7a0b93302b69b8aee094bc23d81da
shyeon0602 <july8560@naver.com>
2024-08-07 22:07:19 +0900

    modified js-calculation directory

---
+++ js/goorm-lecture/js-calculation/script.js
@@ -0,0 +1,13 @@
+const num1 = 20;
+const num2 = 10;
+
+let var1;
+
+// Math Object
+val = Math.E;
+val = Math.PI;
+val = Math.round(2.4); // 반올림
+val = Math.ceil(2.4); // 무조건 올림
+val = Math.floor(2.9); // 무조건 내림
+
+console.log(var1);
