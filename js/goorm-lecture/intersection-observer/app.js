9f79fae654e75d60d7b482b01f4f45cf6b6df8b0
shyeon0602 <july8560@naver.com>
2024-08-12 02:07:44 +0900

    add intersection observer code

---
+++ js/goorm-lecture/intersection-observer/app.js
@@ -0,0 +1,27 @@
+const count = 20;
+let itemIndex = 0;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log("entries ", entries);
+
+    entries.forEach((entry) => {
+      const list = document.querySelector(".list");
+
+      if (entry.isIntersecting) {
+        for (let i = itemIndex; i < itemIndex + count; i++) {
+          let item = document.createElement("p");
+
+          item.textContent = i;
+          item.className += "item";
+          list.appendChild(item); // item이 화면에 보이도록
+        }
+
+        itemIndex = itemIndex + count;
+      }
+    });
+  },
+  { root: null, threshold: 0.1 }
+);
+
+observer.observe(document.querySelector(".end"));
