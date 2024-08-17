c5b2785ed79a65c9c427535f79a79fd55fc5b81d
shyeon0602 <july8560@naver.com>
2024-08-17 22:43:57 +0900

    add event code, fixed createBtnElement eventlistener code

--- js/todo_list/app.js
+++ js/todo_list/app.js
@@ -4,7 +4,7 @@ const todoListElement = document.querySelector("#list");
 let todoList = [];
 let currentId = 0;
 
-createBtnElement.addEventListener("click", createTodo());
+createBtnElement.addEventListener("click", createTodo);
 
 // 새로운 할일 생성
 function createTodo() {
@@ -17,15 +17,15 @@ function createTodo() {
   };
 
   currentId++;
-  console.log(currentId);
+  console.log(`current ID: ${currentId}`);
 
   todoList.unshift(todoItem); // 배열에 새로운 아이템 추가
-  const { todoItemDiv, inputElement, editButton, removeButton } =
-    createTodoElement(todoItem);
+  const { todoItemDiv, inputElement } = createTodoElement(todoItem);
 
   // 리스트 안에 아이템 요소 추가 -> pre는 이미 생성된 아이템 요소 앞에 새로운 요소 생성
   todoListElement.prepend(todoItemDiv);
   inputElement.removeAttribute("disabled");
+  inputElement.focus();
 }
 
 // 할일 요소 생성
@@ -65,5 +65,52 @@ function createTodoElement(todoItem) {
 
   todoItemDiv.append(modifyElement);
 
+  createTodoEvent(
+    todoItem,
+    todoItemDiv,
+    inputElement,
+    checkboxElement,
+    editButton,
+    removeButton
+  );
+
   return { todoItemDiv, inputElement, editButton, removeButton }; // 생성한 요소 반환
+}
+
+function createTodoEvent(
+  todoItem,
+  todoItemDiv,
+  inputElement,
+  checkboxElement,
+  editButton,
+  removeButton
+) {
+  inputElement.addEventListener("input", () => {
+    todoItem.text = inputElement.value;
+  });
+
+  checkboxElement.addEventListener("change", () => {
+    todoItem.complete = checkboxElement.checked;
+
+    if (todoItem.complete) {
+      todoItemDiv.classList.add("complete");
+    } else {
+      todoItemDiv.classList.remove("complete");
+    }
+  });
+
+  inputElement.addEventListener("blur", () => {
+    inputElement.setAttribute("disabled", "");
+  });
+
+  editButton.addEventListener("click", () => {
+    inputElement.removeAttribute("disabled");
+    inputElement.focus();
+  });
+
+  removeButton.addEventListener("click", () => {
+    todoList = todoList.filter((item) => item.id != todoItem.id);
+
+    todoItemDiv.remove();
+  });
 }
