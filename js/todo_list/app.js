ecbfad4d15d56ede7aafbd86fb29762f79f10c84
shyeon0602 <july8560@naver.com>
2024-08-17 21:57:34 +0900

    add create element code

--- js/todo_list/app.js
+++ js/todo_list/app.js
@@ -1,38 +1,67 @@
-const createBtnElement = document.querySelector('#create-btn');
-const todoListElement = document.querySelector('#list');
+const createBtnElement = document.querySelector("#create-btn");
+const todoListElement = document.querySelector("#list");
 
 let todoList = [];
 let currentId = 0;
 
-createBtnElement.addEventListener('click', createTodo);
+createBtnElement.addEventListener("click", createTodo());
 
 // 새로운 할일 생성
 function createTodo() {
-    const todoItem = {
-        id: currentId,
-        text: '',
-        complete: false,
-    }
+  console.log("create todo item");
 
-    currentId++;
-    todoList.unshift(todoItem); // 배열에 새로운 아이템 추가
+  const todoItem = {
+    id: currentId,
+    text: "",
+    complete: false,
+  };
+
+  currentId++;
+  console.log(currentId);
+
+  todoList.unshift(todoItem); // 배열에 새로운 아이템 추가
+  const { todoItemDiv, inputElement, editButton, removeButton } =
     createTodoElement(todoItem);
+
+  todoListElement.prepend(todoItemDiv); // 리스트 안에 아이템 요소 추가
 }
 
 // 할일 요소 생성
 function createTodoElement(todoItem) {
-    const todoItemDiv = document.createElement('div');
-    todoItemDiv.classList.add('item');
-    
-    const checkboxElement = document.createElement('input');
-    checkboxElement.type = 'checkbox';
+  const todoItemDiv = document.createElement("div");
+  todoItemDiv.classList.add("item");
 
-    const inputElement = document.createElement('input');
-    inputElement.type = 'text';
-    inputElement.value = todoItem.text;
-    inputElement.setAttribute('disabled', '');  // 속성 비활성화
+  const checkboxElement = document.createElement("input");
+  checkboxElement.type = "checkbox";
+  checkboxElement.checked = todoItem.complete;
 
-    if(todoItem.complete) {
-        todoItemDiv.classList.add('complete');  // css에서 .complete 스타일 추가
-    }
+  const inputElement = document.createElement("input");
+  inputElement.type = "text";
+  inputElement.value = todoItem.text;
+  inputElement.setAttribute("disabled", ""); // 속성 비활성화, 생성한 후에 입력 방지
+
+  if (todoItem.complete) {
+    todoItemDiv.classList.add("complete"); // css에서 .complete 스타일 추가
+  }
+
+  todoItemDiv.append(checkboxElement);
+  todoItemDiv.append(inputElement);
+
+  const modifyElement = document.createElement("div");
+  modifyElement.classList.add("modify");
+
+  const editButton = document.createElement("button");
+  editButton.classList.add("edit-button");
+  editButton.innerHTML = "edit";
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("remove-button");
+  removeButton.innerHTML = "remove";
+
+  modifyElement.append(editButton);
+  modifyElement.append(removeButton);
+
+  todoItemDiv.append(modifyElement);
+
+  return { todoItemDiv, inputElement, editButton, removeButton }; // 생성한 요소 반환
 }
