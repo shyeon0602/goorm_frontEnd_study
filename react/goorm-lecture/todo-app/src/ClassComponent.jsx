899d5db883254eafe0ef2f2f63aa52cbc63239c9
shyeon0602 <july8560@naver.com>
2024-08-21 13:24:19 +0900

    add 할일 목록 추가 및 삭제 code

--- react/goorm-lecture/todo-app/src/ClassComponent.jsx
+++ react/goorm-lecture/todo-app/src/ClassComponent.jsx
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class ClassComponent extends Component {
+  // state 객체 안에 data 담아주기
+  state = {
+    todoData: [
+      {
+        id: "1",
+        text: "data1",
+        completed: true,
+      },
+      {
+        id: "2",
+        text: "data2",
+        completed: false,
+      },
+    ],
+  };
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -18,18 +34,12 @@ export default class ClassComponent extends Component {
     };
   };
 
-  todoData = [
-    {
-      id: "1",
-      text: "data1",
-      completed: true,
-    },
-    {
-      id: "2",
-      text: "data2",
-      completed: false,
-    },
-  ];
+  // 할일 목록 삭제
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    // setState()를 통해서 todoData를 newTodoData로 상태 바꿔주기
+    this.setState({ todoData: newTodoData });
+  };
 
   render() {
     return (
@@ -40,11 +50,17 @@ export default class ClassComponent extends Component {
             <div className="title">
               <h1>할일 목록</h1>
             </div>
-            {this.todoData.map((data) => (
+            {/* 할일 목록 추가 */}
+            {this.state.todoData.map((data) => (
               <div style={this.getStyle()} key={data.id}>
                 <input type="checkbox" defaultChecked={false} />
                 {data.text}
-                <button style={this.btnStyle}>x</button>
+                <button
+                  style={this.btnStyle}
+                  onClick={() => this.handleClick(data.id)}
+                >
+                  x
+                </button>
               </div>
             ))}
           </div>
