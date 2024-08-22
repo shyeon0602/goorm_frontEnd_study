f702fbab15688d77c5a3a9499e2d6ef2b18441fb
shyeon0602 <july8560@naver.com>
2024-08-22 16:56:52 +0900

    fixed lf replaced crlf error and add Form.jsx file

--- react/goorm-lecture/todo-app/src/components/Form.jsx
+++ react/goorm-lecture/todo-app/src/components/Form.jsx
@@ -1,24 +1,8 @@
 import React from "react";
 
-export default function Form({ value, setTodoData, setValue }) {
+export default function Form({ value, setValue, handleSubmit }) {
   const handleChange = (e) => {
     setValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    // form 안에 input을 전송할 때 페이지 리로드 방지
-    e.preventDefault();
-    // 새로운 할일 데이터
-    let newTodo = {
-      id: Date.now(),
-      text: value,
-      completed: false,
-    };
-
-    // 원래 존재하던 할일에 새로운 할일 추가
-    // setter에서 이전 state를 가지고 오기 위해서 인수에 함수를 이용해서 사용할 수 있음
-    setTodoData((prev) => [...prev, newTodo]);
-    setValue("");
   };
 
   return (
